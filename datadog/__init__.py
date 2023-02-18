d6e3f6cff2a401324b06476714ec1c1e9a514acb
Kyle Verhoog <kyle@verhoog.ca>
2023-02-18 18:56:55 -0500

    Add ASGI middleware

--- datadog/__init__.py
+++ datadog/__init__.py
@@ -1,6 +1,9 @@
+from .asgi import DDASGIMiddleware
 from ._client import DDClient, DDConfig
+
 
 __all__ = [
     "DDClient",
     "DDConfig",
+    "DDASGIMiddleware",
 ]
---
+++ datadog/asgi.py
@@ -0,0 +1,1 @@
+from ddtrace.contrib.asgi import TraceMiddleware as DDASGIMiddleware
